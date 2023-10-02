08eeecf261d4144318e74fe46e92db3248116fb0
101392515 <zain.karedia@georgebrown.ca>
2023-10-01 22:58:14 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const PORT = 2089;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.get('/hello', (req, res) => {
+   res.send('Hello Express JS');
+});
+
+app.get('/user', (req, res) => {
+   const firstname = req.query.firstname || 'Pritesh';
+   const lastname = req.query.lastname || 'Patel';
+   res.json({ firstname, lastname });
+});
+
+app.post('/user/:firstname/:lastname', (req, res) => {
+   const { firstname, lastname } = req.params;
+   res.json({ firstname, lastname });
+});
+
+
+app.listen(PORT, () => {
+   console.log(`Server started on http://localhost:${PORT}`);
+});
